6e5143f2a7ccc2c7210afdd4d355fadd9e3085a6
Slava <rtootik@gmail.com>
2017-03-05 10:46:33 +0300

    Initial Commit

---
+++ 11_lecture_SelectorsTests/_1_lecture_SelectorsTests.m
@@ -0,0 +1,39 @@
+//
+//  _1_lecture_SelectorsTests.m
+//  11_lecture_SelectorsTests
+//
+//  Created by Slava on 05.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _1_lecture_SelectorsTests : XCTestCase
+
+@end
+
+@implementation _1_lecture_SelectorsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
